22f4e86e06b4109a86bff2573fc18e9de1f64b9e
coinsandsteeldev <admin@coinsandsteel.com>
2019-07-09 05:07:01 +0700

    --added events and operations

---
+++ events.js
@@ -0,0 +1,3 @@
+module.exports = {
+    ExperienceChanged: "exp-changed"
+}
---
+++ operations.js
@@ -0,0 +1,10 @@
+module.exports = {
+    Auth: "auth",
+    ChangeNickname: "change_nickname",
+    EngageQuest: "engage_quest",
+    GetZones: "get_zones",
+    GetCharacterInfo: "get_character_info",
+    GetInventory: "get_inventory",
+    ResetStat: "reset_stats",
+    ApplyStats: "apply_stats"
+}
