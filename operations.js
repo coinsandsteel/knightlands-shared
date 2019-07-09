b8cf469c0faf9d622f7f160bf6f94e04fe8ab2d2
coinsandsteeldev <admin@coinsandsteel.com>
2019-07-10 01:29:49 +0700

    --add GetQuestData instead of GetZones

--- operations.js
+++ operations.js
@@ -2,7 +2,7 @@ module.exports = {
     Auth: "auth",
     ChangeNickname: "change_nickname",
     EngageQuest: "engage_quest",
-    GetZones: "get_zones",
+    GetQuestData: "get_quest_data",
     GetUserInfo: "get_user_info",
     ResetStat: "reset_stats",
     ApplyStats: "apply_stats"
