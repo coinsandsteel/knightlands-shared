09aa0e7d8df6d2a43244ba3fbc3b711a01c51993
coinsandsteeldev <admin@coinsandsteel.com>
2019-07-10 06:25:03 +0700

    --added 2 more enums

---
+++ character_stat.js
@@ -0,0 +1,9 @@
+module.exports = {
+    Attack: "attack",
+    Defense: "defense",
+    Health: "health",
+    Energy: "energy",
+    Stamina: "stamina",
+    Luck: "luck",
+    CriticalChance: "criticalChance"
+};
---
+++ upgradable_character_stats.js
@@ -0,0 +1,10 @@
+const CharacterStats = require("./CharacterStats");
+
+module.exports = {
+    Attack: CharacterStats.Attack,
+    Defense: CharacterStats.Defense,
+    Health: CharacterStats.Health,
+    Energy: CharacterStats.Energy,
+    Stamina: CharacterStats.Stamina,
+    Luck: CharacterStats.Luck
+}
