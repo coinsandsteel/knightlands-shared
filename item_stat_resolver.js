a3e542df5bdd041fe5dfe9d6002d676896eef5d8
coinsandsteeldev <admin@coinsandsteel.com>
2019-09-22 00:39:58 +0700

    --support charms

--- item_stat_resolver.js
+++ item_stat_resolver.js
@@ -3,12 +3,14 @@
 const {
     getSlot
   } = require("./equipment_slot");
+const ItemType = require("./item_type");
 
 class ItemStatResolver {
-    constructor(statConversions, itemPower, slotFactors) {
+    constructor(statConversions, itemPower, slotFactors, charmPowerFactor) {
         this._conversions = statConversions;
         this._itemPower = itemPower;
         this._slotFactors = slotFactors;
+        this._charmPowerFactor = charmPowerFactor;
     }
 
     getStatValue(itemRarity, itemSlot, itemLevel, stat, statWeight) {
@@ -24,14 +26,31 @@ class ItemStatResolver {
         return Number.isNaN(value) ? 0 : value;
     }
 
+    getStatValueForCharm(itemRarity, itemLevel, stat, statWeight) {
+        let itemTargetPower = this._itemPower[itemRarity][itemLevel - 1];
+        let statConversionRate = this._conversions[stat];
+        if (statConversionRate === undefined) {
+            statConversionRate = 1;
+        }
+
+        let value = Math.ceil(itemTargetPower * statWeight * this._charmPowerFactor / statConversionRate);
+        return Number.isNaN(value) ? 0 : value;
+    }
+
     convertStats(template, itemLevel) {
         let stats = {};
         const powerFactor = template.powerFactor < 0.001 ? 0 : 1;
 
+        let statFunc;
+        if (template.type == ItemType.Charm) {
+            statFunc = this.getStatValueForCharm.bind(this, template.rarity, itemLevel);
+        } else {
+            statFunc = this.getStatValue.bind(this, template.rarity, getSlot(template.equipmentType), itemLevel);
+        }
+
         for (let idx in template.statWeights) {
             let statWeight = template.statWeights[idx];
-
-            let statValue = this.getStatValue(template.rarity, getSlot(template.equipmentType), itemLevel, statWeight.stat, statWeight.valueWeight) * powerFactor;
+            let statValue = statFunc(statWeight.stat, statWeight.valueWeight) * powerFactor;
             if (statValue != 0) {
                 stats[statWeight.stat] = statValue;
             }
