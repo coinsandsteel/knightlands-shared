de774c22330760d4d7e5507e586ffe68c132d5ae
coinsandsteeldev <admin@coinsandsteel.com>
2019-10-05 09:30:08 +0700

    --resource training cost

--- training_camp.js
+++ training_camp.js
@@ -2,18 +2,23 @@ import UpgradableStats from "./upgradable_character_stats";
 
 export default {
     getStatCost(stat, level) {
+        return 500 + 250 * level;
+    },
+
+    getStatResourceCost(stat, level) {
         level += 1;
+
         switch (stat) {
             case UpgradableStats.Health:
-                return Math.ceil(Math.pow(level, 2) * 2.3);
+                return Math.ceil(Math.pow(level, 0.68) * 1.1);
             case UpgradableStats.Attack:
-                return Math.ceil(Math.pow(level, 2.1) * 3);
+                return Math.ceil(Math.pow(level, 0.75) * 1.1);
             case UpgradableStats.Luck:
-                return Math.ceil(Math.pow(level, 2) * 2);
+                return Math.ceil(Math.pow(level, 0.6) * 1.1);
             case UpgradableStats.Energy:
-                return Math.ceil(Math.pow(level, 2.4) * 2);
+                return Math.ceil(Math.pow(level, 0.83) * 1.1);
             case UpgradableStats.Stamina:
-                return Math.ceil(Math.pow(level, 2.6) * 2);
+                return Math.ceil(Math.pow(level, 0.9) * 1.1);
         }
     },
 
