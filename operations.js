d639008d0a989aa60bd809cb8f6930051a308447
coinsandsteeldev <admin@coinsandsteel.com>
2020-03-22 23:36:17 +0700

    --disenchanting convert command

--- operations.js
+++ operations.js
@@ -81,5 +81,6 @@ module.exports = {
     ClaimDailyTasksRewards: "claim_daily_tasks_rewards",
     WithdrawDividendToken: "withdraw_div_token",
     FetchPendingDividendTokenWithdrawal: "fetch_pending_div_token_withdrawal",
-    SendDividendTokenWithdrawal: "send_div_token_withdrawal"
+    SendDividendTokenWithdrawal: "send_div_token_withdrawal",
+    DisenchantConvert: "dis_convert"
 }
