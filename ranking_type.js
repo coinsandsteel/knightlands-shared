6ead7af8e29c14f36188c58e25727fe3375677d2
coinsandsteeldev <admin@coinsandsteel.com>
2020-04-05 12:57:06 +0700

    ranking types enum

---
+++ ranking_type.js
@@ -0,0 +1,13 @@
+export default {
+    EnergySpent: 0,
+    StaminaSpent: 1,
+    DamageInRaids: 2,
+    ExpGained: 3,
+    GoldLooted: 4,
+    DktEarned: 5,
+    GoldSpent: 6,
+    DamageInParticularRaid: 7,
+    CollectedItemsByRarity: 8,
+    CraftedItemsByRarity: 9,
+    DisenchantedItemsByRarity: 10
+}
