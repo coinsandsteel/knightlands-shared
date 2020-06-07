37412e70cc77f8893b1e3de6fa73146e6951b892
coinsandsteeldev <admin@coinsandsteel.com>
2020-06-07 23:05:49 +0700

    fix: use troops meta

--- ability_resolver.js
+++ ability_resolver.js
@@ -7,9 +7,10 @@ class AbilityResolver {
     }
 
     getAbilityLevelValue(unitStars, abilityId, isTroop) {
-        const abilityRecords = isTroop ? this._meta.generals : this._meta.generals;
+        const abilityRecords = isTroop ? this._meta.troops : this._meta.generals;
         const abilityTemplate = abilityRecords[abilityId];
         const levelRecord = abilityTemplate.levels[unitStars - 1];
+        if (!levelRecord) return 0;
         let value = {};
         if (levelRecord.power) {
             value.damage = this._statResolver.powerToStatValue(levelRecord.power, CharacterStat.Attack);
