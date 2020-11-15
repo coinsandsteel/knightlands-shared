2605c80e836119ca4afe72c9f308399cdf3827dd
coinsandsteeldev <admin@coinsandsteel.com>
2020-11-15 18:25:19 +0700

    new: accessory option enum

---
+++ accessory_option.js
@@ -0,0 +1,14 @@
+export default {
+    DropItemInQuest: "dropItemInQuest",
+    DropItemInRaid: "dropItemInRaid",
+    DropUnitShard: "dropUnitShard",
+    IncreasedDktInRaid: "increasedDktInRaid",
+    GoldOnHitInRaid: "goldOnHitInRaid",
+    ExpOnHitInRaid: "expOnHitInRaid",
+    ArmyDamageInRaidElement: "armyDamageInRaidElement",
+    Dodge: "dodge",
+    RewardsOnyxTower: "rewardsOnyxTower",
+    RewardsAdventures: "rewardsAdventures",
+    RewardsTrial: "rewardsTrial",
+    IncreasedStat: "increasedStat"
+}
