7c90f71933b9de9bd5f3b96b820ecdcf8d58710d
coinsandsteeldev <admin@coinsandsteel.com>
2021-01-16 16:55:48 +0700

    fix: new stats formula

--- training_camp.js
+++ training_camp.js
@@ -6,20 +6,7 @@ export default {
     },
 
     getStatResourceCost(stat, level) {
-        level += 1;
-
-        switch (stat) {
-            case UpgradableStats.Health:
-                return Math.ceil(Math.pow(level, 0.68) * 1.1);
-            case UpgradableStats.Attack:
-                return Math.ceil(Math.pow(level, 0.75) * 1.1);
-            case UpgradableStats.Luck:
-                return Math.ceil(Math.pow(level, 0.6) * 1.1);
-            case UpgradableStats.Energy:
-                return Math.ceil(Math.pow(level, 0.83) * 1.1);
-            case UpgradableStats.Stamina:
-                return Math.ceil(Math.pow(level, 0.9) * 1.1);
-        }
+        return Math.ceil(Math.pow(level + 1, 0.4) * 1.1);
     },
 
     getMaxStat(characterLevel) {
