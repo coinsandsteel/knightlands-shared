cf1fa6ded0e2d73165b9cf53ed65f3553a18e5f2
imyourm8 <imyourm9@gmail.com>
2021-11-17 14:25:03 +0700

    new: withdraw USDC in dungeon cmd

--- operations.js
+++ operations.js
@@ -170,4 +170,5 @@ module.exports = {
     SDungeonRank: "sdungeon_rank",
     SDungeonEnter: "sdungeon_enter",
     SDunegonCommitStats: "sdungeon_commit_stats",
+    SDungeonWithdraw: "sdungeon_withdraw"
 }
