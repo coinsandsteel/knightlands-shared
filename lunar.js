e26cd456c92ef7ac7b171ec6a7d74b267d97b9e0
Phuc Le <lehuuphucaptech@gmail.com>
2022-01-14 23:01:02 +0700

    lunar exchange page

--- lunar.js
+++ lunar.js
@@ -1,7 +1,4 @@
-import _ from 'lodash';
-
 export const ITEM_RARITY_BASIC = 'common';
 export const ITEM_RARITY_ADVANCED = 'rare';
 export const ITEM_RARITY_EXPERT = 'epic';
 export const ITEM_RARITY_NFT = 'legendary';
-
