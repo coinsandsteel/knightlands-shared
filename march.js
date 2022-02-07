b99700b33ef2ac15986527fa6b84aeed42938310
uniwertz <uniwertz@gmail.com>
2022-02-07 15:47:18 +0300

    chg: Type > class

--- march.js
+++ march.js
@@ -1,21 +1,22 @@
+export const UNIT_CLASS_PET = 'pet';
+
 // Artifacts
-export const UNIT_TYPE_BALL_LIGHTNING = 'ball_lightning';
-export const UNIT_TYPE_DRAGON_BREATH = 'dragon_breath';
-export const UNIT_TYPE_BOMB = 'bomb';
-export const UNIT_TYPE_REINFORCER = 'reinforcer';
+export const UNIT_CLASS_BALL_LIGHTNING = 'ball_lightning';
+export const UNIT_CLASS_DRAGON_BREATH = 'dragon_breath';
+export const UNIT_CLASS_BOMB = 'bomb';
 
 // Containers
-export const UNIT_TYPE_CHEST = 'chest';
-export const UNIT_TYPE_BARRELL = 'barell';
+export const UNIT_CLASS_CHEST = 'chest';
+export const UNIT_CLASS_BARRELL = 'barell';
 
 // Enemies
-export const UNIT_TYPE_GUARD = 'guard';
-export const UNIT_TYPE_BOSS = 'boss';
-export const UNIT_TYPE_TRAP = 'trap';
+export const UNIT_CLASS_ENEMY = 'enemy';
+export const UNIT_CLASS_ENEMY_BOSS = 'enemy_boss';
+export const UNIT_CLASS_TRAP = 'trap';
 
 // Loot
-export const UNIT_TYPE_HP = 'hp';
-export const UNIT_TYPE_EXTRA_HP = 'extra_hp';
-export const UNIT_TYPE_ARMOR = 'armor';
-export const UNIT_TYPE_BOW = 'bow';
-export const UNIT_TYPE_GOLD = 'gold';
+export const UNIT_CLASS_HP = 'hp';
+export const UNIT_CLASS_EXTRA_HP = 'extra_hp';
+export const UNIT_CLASS_ARMOR = 'armor';
+export const UNIT_CLASS_BOW = 'bow';
+export const UNIT_CLASS_GOLD = 'gold';
