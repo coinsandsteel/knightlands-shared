f25ed076a56a45a49181ccccbd995b6af451b38d
uniwertz <uniwertz@gmail.com>
2022-02-23 19:14:16 +0400

    chg: Const naming

--- march.js
+++ march.js
@@ -1,8 +1,8 @@
 export const UNIT_CLASS_PET = 'pet';
 
 // Artifacts
-export const UNIT_CLASS_BALL_LIGHTNING = 'ball_lightning';
-export const UNIT_CLASS_DRAGON_BREATH = 'dragon_breath';
+export const UNIT_CLASS_BALL_LIGHTNING = 'ballLightning';
+export const UNIT_CLASS_DRAGON_BREATH = 'dragonBreath';
 export const UNIT_CLASS_BOMB = 'bomb';
 export const UNIT_CLASS_BOW = 'bow';
 
@@ -12,12 +12,12 @@ export const UNIT_CLASS_BARREL = 'barrel';
 
 // Enemies
 export const UNIT_CLASS_ENEMY = 'enemy';
-export const UNIT_CLASS_ENEMY_BOSS = 'enemy_boss';
+export const UNIT_CLASS_ENEMY_BOSS = 'enemyBoss';
 export const UNIT_CLASS_TRAP = 'trap';
 
 // Loot
 export const UNIT_CLASS_HP = 'hp';
-export const UNIT_CLASS_EXTRA_HP = 'extra_hp';
+export const UNIT_CLASS_EXTRA_HP = 'extraHp';
 export const UNIT_CLASS_ARMOR = 'armor';
 export const UNIT_CLASS_GOLD = 'gold';
 
